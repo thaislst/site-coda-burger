d38d16ae9789a890ae97d7a4263a1865eea455b3
thaislst <89035755+thaislst@users.noreply.github.com>
2022-06-06 17:15:18 -0300

    remoção de códigos não necessários

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 "use strict";
 
-console.log('roodei');
-var navigation = document.getElementById("navigation")
+
+window.addEventListener('scroll', onScroll)
+
+onScroll()
 
 function onScroll() {
   showNavOnScroll(); 
